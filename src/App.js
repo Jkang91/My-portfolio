1da8751d126286841be21368ff56f6e5715e1cad
jkang91 <jkang34@gmail.com>
2021-04-28 22:21:12 -0400

    Contacts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Experience from "./components/Experience";
 import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact"
 
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
       <AboutMe />
       <Experience />
       <Portfolio />
+      <Contact />
     </>
   );
 }
